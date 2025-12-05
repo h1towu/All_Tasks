2b8315c3603516f082035b8e44e0473425e24e55
Egor <egprohorenko@mail.ru>
2025-12-05 17:10:21 +0300

    add coffee

---
+++ Python/coffee/main.py
@@ -0,0 +1,25 @@
+class Drink:
+    def __init__(self, name, type, cost, volume):
+        self.name = name
+        self.type = type
+        self.cost = cost
+        self.volume = volume
+        
+    def show_info(self):
+        return f"Информация о напитках: {self.name} {self.type} {self.cost} {self.volume}"
+    def change_cost(self):
+        pass
+    
+capuccino = Drink('Капучино', 'кофе', 200, 'M')
+
+class Order:
+    def __init__(self, number, list, client, status, endCost):
+        self.number = number
+        self.list = list
+        self.client = client
+        self.status = status
+        self.endCost = endCost
+        
+    def add_drink(self, drink):
+        list = ()
+        
